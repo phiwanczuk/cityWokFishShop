afec08ef70ed575079e904a353b5b4787b44d5dd
Piotrek <phiwanczuk@gmail.com>
2018-01-29 17:02:39 +0100

    pointing onto this with constructor or bind method

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import {getFunName} from "../helpers";
 
 
 class StorePicker extends React.Component{
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this)
+    // }
+
+    goToStore(event){
+        event.preventDefault();
+        console.log('You changed the URL');
+        // first grab the text from the box
+        console.log(this);
+        // then we re going to transition from / to/ store/:storeId
+    }
+
     render(){
         return(
-            <form className="store-selctor">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
             <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name"/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=>{this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         ) 
     }
 }
+
 
 export default StorePicker;
